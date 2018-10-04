423dd18995ba250985d992e9cc6f59e17cf8878c
svmihar <svmihar@gmail.com>
2018-10-04 17:50:11 +0700

    asistensi minggu ke 3

---
+++ 3_inheritance/INHERITANCE/src/inheritance/INHERITANCE.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+
+class kalkulator{
+    public int tambah (int x, int y){
+        return x+y; 
+    }
+}
+
+class minus{
+    public int tambah (int x, int y){
+        return x+y; 
+    }
+    public int kurang(int x, int  y ){
+        return x-y;
+    }
+}
+
+public class INHERITANCE {
+     public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
